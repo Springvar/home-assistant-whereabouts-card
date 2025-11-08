e77709a36aa2cea7d2505eea468b8e5bec0a543a
Ivar Bonsaksen <ivar.bonsaksen@gmail.com>
2025-11-08 13:13:48 +0100

    Satisfy "One of the .js files has the same name as the repository." hacs requirement

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/whereabouts-card.ts',
-      formats: ['es'],
-      fileName: 'home-assistant-whereabouts-card'
-    },
-    outDir: 'dist',
-    rollupOptions: {
-      external: [],
-    },
-    minify: 'esbuild',
-  }
+    build: {
+        lib: {
+            entry: 'src/whereabouts-card.ts',
+            formats: ['es'],
+            fileName: 'home-assistant-whereabouts-card'
+        },
+        outDir: 'dist',
+        rollupOptions: {
+            external: [],
+            output: {
+                entryFileNames: 'home-assistant-whereabouts-card.js'
+            }
+        },
+        minify: 'esbuild'
+    }
 });
